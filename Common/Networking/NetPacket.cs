6a8651d9dcaee79d33c73e50e439e0c3cbeb683d
Bread <97198020+Breadiguess@users.noreply.github.com>
2026-05-28 10:46:15 -0600

    Added Packet System

---
+++ Common/Networking/NetPacket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace Mycorrhiza.Common.Networking
+{
+    public abstract class NetPacket : ModType
+    {
+        protected sealed override void Register()
+        {
+            PacketLoader.Register(this);
+        }
+        public abstract byte PacketID { get; internal set; }
+
+        public virtual void Write(BinaryWriter writer) { }
+
+        public virtual void Read(BinaryReader reader) { }
+
+        public abstract void Handle(int whoAmI);
+
+        public void Send(int toClient = -1, int ignoreClient = -1)
+        {
+            if (Main.netMode == Terraria.ID.NetmodeID.SinglePlayer)
+                return;
+
+            ModPacket packet = ModContent.GetInstance<Mycorrhiza>().GetPacket();
+
+            packet.Write(PacketID);
+            Write(packet);
+
+            packet.Send(toClient, ignoreClient);
+        }
+    }
+}
--- Mycorrhiza.cs
+++ Mycorrhiza.cs
@@ -1,5 +1,7 @@
+using Mycorrhiza.Common.Networking;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,21 @@ using Terraria.ModLoader;
 namespace Mycorrhiza
 {
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class Mycorrhiza : Mod
+	public partial class Mycorrhiza : Mod
 	{
+        public override void Load()
+        {
+            
+        }
+        public override void Unload()
+        {
+            PacketLoader.Unload();
+        }
 
+
+        public override void HandlePacket(BinaryReader reader, int whoAmI)
+        {
+            NetLinker.HandlePacket(reader, whoAmI);
+        }
 	}
 }
